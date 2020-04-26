1ec3126f14580aaf2c1f154803176210e062ad53
Sauleee <64333667+Sauleee@users.noreply.github.com>
2020-04-26 21:46:52 +0500

    Update main.py

--- ENDTERM/PROJECT1/main.py
+++ ENDTERM/PROJECT1/main.py
@@ -7,16 +7,16 @@ game = GameState()
 
 
 while True:
-    for i in pygame.event.get():   #evens содержит список событии
-        if i.type == pygame.QUIT:  #цикл обработки событии
-            pygame.quit()  #завершает
+    for i in pygame.event.get():  
+        if i.type == pygame.QUIT:  
+            pygame.quit()
             break
 
-    keys = pygame.key.get_pressed()  #снимает маску  зажатых клавиш
+    keys = pygame.key.get_pressed()  
     game.process()    
     game.hero.move(keys)   
     game.draw()  
     
 
-    pygame.display.flip()  #обновление экрана
+    pygame.display.flip() 
 
